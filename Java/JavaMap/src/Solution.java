580cee000463e6e8ebb1451276452884ac6439f7
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-04 18:38:04 +0700

    Add Java Map challenges solution

--- Java/JavaMap/src/Solution.java
+++ Java/JavaMap/src/Solution.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.Scanner;
 
 class Solution {
-    public static void main(String[] argh) {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         in.nextLine();
